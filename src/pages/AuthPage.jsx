d3dc499fe004343cb0276d9bdf658cd9006567b9
Muhammad Tayyab Rizwan <tayyabprince193@gmail.com>
2025-06-05 15:12:01 +0500

    Fixed bg color on invalid login credentials

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -202,7 +202,7 @@ function AuthPage() {
                     <label
                       className={`flex flex-col items-center justify-center p-4 border rounded-lg cursor-pointer transition-all ${
                         formData.role === 'guest'
-                          ? 'border-green-500 bg-green-50'
+                          ? 'border-green-500 bg-red-50'
                           : 'border-gray-200 hover:border-gray-300'
                       }`}
                     >
